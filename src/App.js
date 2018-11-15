6c2fa70ad44efaf48246b206709bc7ba559948ca
steven2493 <stevenaguilar24@gmai.com>
2018-11-15 15:44:18 -0500

    Added hangman images to be generated when user clicks the wrong letter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
+import hangman_0 from './hangman-images/Hangman-0.png';
+import hangman_1 from './hangman-images/Hangman-1.png';
+import hangman_2 from './hangman-images/Hangman-2.png';
+import hangman_3 from './hangman-images/Hangman-3.png';
+import hangman_4 from './hangman-images/Hangman-4.png';
+import hangman_5 from './hangman-images/Hangman-5.png';
+import hangman_6 from './hangman-images/Hangman-6.png';
 
 //Place holder before implementing future feature
-const word = "steven";
+const wordOfTheGame = "steven";
 
 //Word of the game that users have to guess the correct letter to find the right answer
 class Word extends Component{
@@ -10,39 +16,85 @@ class Word extends Component{
     super(props);
     this.state = {
       wordScrambled:this.wordToGuess(),
+      wordIsGuessed: false,
+      numOfGuess: 0,
+      hangman: hangman_0
     }
     this.clickEvent = this.clickEvent.bind(this);
   }
 
   wordToGuess() {
-    let wordGuess = new Array(word.length);
-    for(let i = 0; i < word.length; i++){
+    let wordGuess = new Array(wordOfTheGame.length);
+    for(let i = 0; i < wordOfTheGame.length; i++){
       wordGuess[i] = "_"
     }
     return wordGuess.join(' ');
   }
   
+  checkWord(){
+    if(this.state.wordScrambled.split(" ").join("") === wordOfTheGame){
+      this.setState({wordIsGuessed: true})
+    }else{
+      console.log("checkword function: false")
+    }
+  }
+
+  updateHangman(){
+    if(this.state.numOfGuess == 1){
+      console.log(`True: ${this.state.hangman}`);
+      this.setState({hangman:hangman_1});
+    }else if(this.state.numOfGuess == 2){
+      this.setState({hangman:hangman_2});
+    }else if(this.state.numOfGuess == 3){
+      this.setState({hangman:hangman_3});
+    }else if(this.state.numOfGuess == 4){
+      this.setState({hangman:hangman_4});
+    }else if(this.state.numOfGuess == 5){
+      this.setState({hangman:hangman_5});
+    }else if(this.state.numOfGuess == 6){
+      this.setState({hangman:hangman_6});
+    }
+  }
+
   clickEvent(letter){
-    //Last point working on, button now has function that prints the buttons letter to console log.
-    console.log(`state.wordScrambled ${this.state.wordScrambled}`)
-    const x = this.state.wordScrambled.split(" ");
-    console.log(` Variable x at Sart of Function ${x}`);
-    if(word.indexOf(letter) > -1){
-      x.map((i, y) => {
-        if(word[y] === letter){
-          x[y] = letter;
+    //Checks if the word has been solved each time user pressses a button.
+    this.checkWord();
+    if(this.state.wordIsGuessed === true){
+      //Add logic when game is finshed
+      console.log("Done");
+      return;
+    }
+    
+    //Check if the button press is a valid answer if yes, then update this.state.wordScrambled.
+    const wordCopy = this.state.wordScrambled.split(" ");
+    
+    if(wordOfTheGame.indexOf(letter) > -1){
+      wordCopy.map((i, y) => {
+        if(wordOfTheGame[y] === letter){
+          wordCopy[y] = letter;
+          console.log(`Num of Guess: ${this.state.numOfGuess}`);
         }
       });
+    }else{
+      //updates the number of guesses
+      this.setState({
+        count: this.state.numOfGuess++,
+      })
+      console.log(`Num of Guess: ${this.state.numOfGuess}`);      
+      this.updateHangman();      
     }
+
+    //update hidden word state after user guess
     this.setState({
-      wordScrambled: x.join(" "),
+      wordScrambled: wordCopy.join(" "),
     })
-    console.log(` Variable x at End of Function ${x}`);
+    console.log(`Num of Guess: ${this.state.numOfGuess}`);
   }
 
   render(){
     return(
       <div>
+      <img  src={this.state.hangman} alt="hangman_0"/>
       <p>{this.state.wordScrambled}</p>
       <WordBank buttonPressed={this.clickEvent}/>  
       </div>
